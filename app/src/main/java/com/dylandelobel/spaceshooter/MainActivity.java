13e12f1d57a92504321f52d25cc9557b729340ac
Dylan Delobel <dylan.delobel66@gmail.com>
2017-01-26 20:29:27 +0100

    Add Main menu

--- app/src/main/java/com/dylandelobel/spaceshooter/MainActivity.java
+++ app/src/main/java/com/dylandelobel/spaceshooter/MainActivity.java
@@ -1,13 +1,28 @@
 package com.dylandelobel.spaceshooter;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onPlayButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(), "Hello man!!!", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
